12951f77a2d021038763ec56319cd08c09cbefcb
Hariom Verma <hariom18599@gmail.com>
2018-12-26 02:25:01 +0530

    few changes form.py

--- pro1/users/forms.py
+++ pro1/users/forms.py
@@ -10,12 +10,23 @@ class UserRegisterForm(UserCreationForm):
 	email = forms.EmailField(required = True)
 	first_name = forms.CharField(max_length = 32, required = True)
 	last_name = forms.CharField(max_length = 32, required = True)
+	branch = forms.ChoiceField(choices=[
+		(1, 'Computer Science and Engineering'), 
+		(2, 'Electronics and Communication Engineering'),
+		(3, 'Electrical Engineering'),
+		(4, 'Metallurgical and Materials Engineering'),
+		(5, 'Biotechnology'),
+		(6, 'Information Technology'),
+		(7, 'Civil Engineering'),
+		(8, 'Chemical Engineering'),
+		])
+
 	#default is true that it is required
 	# add validation for institute email id
 
 	class Meta:
 		model = User
-		fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']  # in-order
+		fields = ['username', 'email', 'first_name', 'last_name', 'branch', 'password1', 'password2']  # in-order
 
 	def clean_email (self) :
 		data = self.cleaned_data['email']
